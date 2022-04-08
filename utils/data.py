99a55f2ef89f033fb977ecacdb54f042abd3edf9
cdelavegamartin <c.delavega.martin@protonmail.com>
2022-04-08 13:38:18 +0100

    MDB-stem-synth dataset. Preprocessing for input to a model is done separately in training script

---
+++ utils/data.py
@@ -0,0 +1,54 @@
+from __future__ import print_function, division
+import os
+import torch
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+import librosa
+import wave
+
+
+
+
+class MdBStemSynthDataset(Dataset):
+    def __init__(self, wav_dir, annot_dir, transform=None):
+        self.wav_dir = wav_dir
+        self.annot_dir = annot_dir
+
+        wav_count = sum(1 for file in os.listdir(wav_dir) if (file.endswith(".wav") and not file.startswith("._")))
+        annot_count = sum(1 for file in os.listdir(annot_dir) if file.endswith(".csv"))
+
+        if wav_count == annot_count:
+            self.data_fnames = [os.path.splitext(file)[0]  for file in os.listdir(annot_dir) if file.endswith(".csv")]
+        else:
+            print("number of audio files and annotations do not match")
+
+
+    def __len__(self):
+        return len(self.data_fnames)
+
+
+    def __getitem__(self,idx):
+        
+
+        audio, samplerate = librosa.load(os.path.join(self.wav_dir, self.data_fnames[idx])+".wav",sr=None)
+        pitch_annotation= pd.read_csv(os.path.join(self.annot_dir, self.data_fnames[idx])+".csv",header=None).to_numpy()
+
+        # print(wave.open(os.path.join(self.wav_dir, self.data_fnames[idx])+".wav").getframerate())
+
+        sample = {'name': self.data_fnames[idx], 'audio': audio, 'samplerate': samplerate, 'pitch': pitch_annotation}
+        
+        return sample
+
+
+
+if __name__ == "__main__":
+
+
+    mdb_dataset = MdBStemSynthDataset("datasets/MDB-stem-synth/audio_stems","datasets/MDB-stem-synth/annotation_stems")
+    print(mdb_dataset.data_fnames[0])
+    dat = mdb_dataset[0]
+    print(type(dat[0]))
+    print(dat.shape)
+    
