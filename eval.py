29f804291ccd3b3cace03915f516723b34c3a6fe
cdelavegamartin <c.delavega.martin@protonmail.com>
2022-04-15 17:32:36 +0100

    Evaluation script

---
+++ eval.py
@@ -0,0 +1,21 @@
+import torch
+import mir_eval
+import sklearn
+import crepe
+
+
+def extract_pitch_crepe():
+    pass
+
+def calculate_rpa(true_pitch, predicted_pitch):
+
+    # true pitch and predicted pitch 
+    pass
+
+def evaluate_pitch_accuracy(trained_model,threshold):
+
+    # load test dataset
+
+    # load trained model
+
+    pass
