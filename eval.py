ba7e04772da5b3c62537689bb49199b694bdb11c
cdelavegamartin <c.delavega.martin@protonmail.com>
2022-04-17 03:08:37 +0100

    Fix double application of sigmoid

--- eval.py
+++ eval.py
@@ -183,7 +183,7 @@ def evaluate_pitch(trained_model, dataset_dir='datasets/MDB-stem-synth/', gpu_id
     # print(list(dataset.instrument_id_map.keys())[list(dataset.instrument_id_map.values()).index(0)])
     
     # Loss function
-    loss = torch.nn.BCEWithLogitsLoss()
+    loss = torch.nn.BCELoss()
     
     # Eval parameters
     # batch_size = len(dataset)
@@ -325,7 +325,7 @@ def evaluate_instclass(trained_model, dataset_dir='datasets/nsynth/', gpu_id="")
     # print(list(dataset.instrument_id_map.keys())[list(dataset.instrument_id_map.values()).index(0)])
     
     # Loss function
-    loss = torch.nn.BCEWithLogitsLoss()
+    loss = torch.nn.BCELoss()
     
     # Eval parameters
     batch_size = len(dataset)
@@ -453,7 +453,7 @@ def evaluate_combined(trained_model, dataset_dir='datasets/nsynth/', gpu_id="", 
     # print(list(dataset.instrument_id_map.keys())[list(dataset.instrument_id_map.values()).index(0)])
     
     # Loss function
-    loss = torch.nn.BCEWithLogitsLoss()
+    loss = torch.nn.BCELoss()
     
     # Eval parameters
     # batch_size = len(dataset)
